98d73968bd3af13176a37cdfca42ef797fa233e8
KEINA0109O\Keina_S <osamu.engineer.2001@gmail.com>
2023-04-18 21:53:54 +0900

    新規作成の画面遷移

--- src/App.js
+++ src/App.js
@@ -2,16 +2,34 @@ import "./App.css";
 import React from "react";
 import ContentArea from "./ContentArea";
 import NewAreaButton from "./NewArea";
-function textTop() {
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import NewTodo from "./NewTodo";
+import { useNavigate } from "react-router-dom";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={`/`} element={<TextTop />} />
+        <Route path={`/new/`} element={<NewTodo />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+function TextTop() {
+  const navigate = useNavigate();
   return (
     <div>
       <h1 style={{ paddingLeft: 20 }}>ToDo</h1>
       <ContentArea></ContentArea>
       <div style={{ textAlign: `right` }}>
-        <button style={NewAreaButton}>新規作成</button>
+        <button style={NewAreaButton} onClick={() => navigate("/new")}>
+          新規作成
+        </button>
       </div>
     </div>
   );
 }
 
-export default textTop;
+export default App;
---
+++ src/NewTodo.js
@@ -0,0 +1,8 @@
+import { BrowserRouter } from "react-router-dom";
+import "./App.css";
+
+const NewTodo = () => {
+  return <p>nyohoooo</p>;
+};
+
+export default NewTodo;
